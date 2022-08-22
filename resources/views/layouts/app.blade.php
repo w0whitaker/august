0d378516c566e3c680fab6eee1fd698bbbdd72ba
William Whitaker <billydwhitaker@gmail.com>
2022-08-22 16:30:00 -0400

    innit that commit!

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<a class="sr-only focus:not-sr-only" href="#main">
+  {{ __('Skip to content') }}
+</a>
+
+@include('sections.header')
+
+  <main id="main" class="main">
+    <h1>william whitaker</h1>
+    @yield('content')
+  </main>
+
+  @hasSection('sidebar')
+    <aside class="sidebar">
+      @yield('sidebar')
+    </aside>
+  @endif
+
+@include('sections.footer')
